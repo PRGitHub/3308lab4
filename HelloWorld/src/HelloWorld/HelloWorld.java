e5c6b0c09f5f77d2a4069c14610cf6b3fad28276
PRGitHub <PRGitHub@users.noreply.github.com>
2014-09-17 13:52:51 -0600

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string
 		System.out.println("How are you?");
+		System.out.println("Adding a new commit on GitHub for Laxit to see");
 	}
 
 }
