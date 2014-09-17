5a219c3d8850a1e8455bbbd7d143c610f8861050
PRGitHub <PRGitHub@users.noreply.github.com>
2014-09-17 13:38:42 -0600

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package HelloWorld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string
+		System.out.println("How are you?");
 	}
 
 }
